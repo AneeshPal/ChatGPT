a03ec76fec9c836dbc1e848bdcf4bee782206594
AneeshPal <2k23.cs2313398@gmail.com>
2026-09-05 16:11:26 +0530

    added the react-spinner till we get response from model

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,28 +1,69 @@
 import "./ChatWindow.css";
+import { useState } from 'react';
 import Chat from "./Chat.jsx";
 
+import { useContext } from "react";
+import {MyContext} from "./MyContext.jsx"
+import {ScaleLoader} from "react-spinners";
+
 function ChatWindow() {
+    const {prompt,setPrompt,reply,setReply,currThreadId}=useContext(MyContext);
+    const [loading,setLoading]=useState(false);
+
+    const getReply=async ()=>{
+        setLoading(true);
+        console.log("meesage",prompt,"threadId",currThreadId);
+        const options={
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                message:prompt,
+                threadId:currThreadId
+            })
+        };
+
+        try{
+            const response=await fetch("http://localhost:8080/api/chat",options)
+            const res=await response.json();
+            setReply(res.reply);
+            console.log(res);
+        }catch(err){
+            console.log(err);
+        }
+
+        setLoading(false);
+    }
+
     return (
         <div className="chatWindow">
             <div className="navbar">
-                <span >SigmaGPT <i class="fa-solid fa-angle-down"></i></span>
+                <span >AneeshGPT <i className="fa-solid fa-angle-down"></i></span>
                 <div className="userIconDiv">
                     <span className="userIcon">
-                        <i class="fa-solid fa-user"></i>
+                        <i className="fa-solid fa-user"></i>
                     </span>
                 </div>
             </div>
+            <Chat></Chat>
 
+            {/* react-spinner */}
+            <ScaleLoader color='#fff' loading={loading}>
+
+            </ScaleLoader>
             <div className="chatInput">
                 <div className="inputBox">
-                    <input placeholder="Ask Anything">
+                    <input placeholder="Ask Anything" value={prompt}
+                        onChange={(e)=> setPrompt(e.target.value)}
+                        onKeyDown={(e)=> e.key ==='Enter' ?getReply():''}>
                     </input>
-                    <div id="submit">
-                        <i class="fa-solid fa-paper-plane"></i>
+                    <div id="submit" onClick={getReply}>
+                        <i className="fa-solid fa-paper-plane"></i>
                     </div>
                 </div>
                 <p className="info">
-                    SigmaGPT can make mistakes.Check important info.See Cookie Preferences.
+                    AneeshGPT can make mistakes.Check important info.See Cookie Preferences.
                 </p>
 
             </div>
