01c07b6bd1b84a6af8f0a4793091b35e8b92c343
AneeshPal <2k23.cs2313398@gmail.com>
2026-09-13 20:56:40 +0530

    added the history rendering based on thread title

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,7 +1,27 @@
 import "./Sidebar.css";
-
+import {useContext, useEffect} from "react";
+import {MyContext} from "./MyContext.jsx"
 
 function Sidebar (){
+
+    const {allThreads,setAllThreads,currThread}=useContext(MyContext);
+
+    const getAllThreads=async()=>{
+        try{
+            const response=await fetch("http://localhost:8080/api/thread");
+            const res=await response.json();
+            const filteredData=res.map( thread =>({threadId:thread.threadId,title:thread.title}));
+            console.log(filteredData);
+            setAllThreads(filteredData);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    useEffect(()=>{
+        getAllThreads();
+    },[currThread])
+
     return (
         <section className="sidebar">
            { /* new chat button*/}
@@ -12,9 +32,15 @@ function Sidebar (){
 
            { /* history*/}
             <ul className="history">
-                <li>thread1</li>
-                <li>thread2</li>
-                <li>thread3</li>
+
+               {/* DISPLAYING HISTORY */}
+
+               {
+                  allThreads.map((thread,idx)=>(
+                    <li key={idx}>{thread.title}</li>
+                  ))
+               }
+
             </ul>
             
             { /* sign */}
